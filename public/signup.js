c1611b9acac58bacbbe3a5a790454117fb255204
Helen Tran <helentran@helens-mbp.attlocal.net>
2020-12-02 19:46:28 -0500

    fixed signup, created login, attempted user auth

--- public/signup.js
+++ public/signup.js
@@ -1,75 +1,42 @@
-import { auth } from 'google-auth-library';
-
-const {admin, db, firebase, users} = require('./backend/firebase');
-
-export const signUpDisplay = function() {
-    let signUp = $('<div class = "signup"><h1>Sign Up</h1>');
-    let form = $('<div id="signupForm">');
-    let content =  $('<form></form>');
-
-    let first = '<h1>First Name</h1><input id="first" type="text" placeholder="Your First Name" required>';
-    let last = '<h1>Last Name</h1><input id="last" type="text" placeholder="Your Last Name" required>';
-    let email = '<h1>Email</h1><input id = "email" type="text" placeholder="123@gmail.com" required>';
-    let username = '<h1>Username</h1><input id= "username" type="text" placeholder="user123" required>';
-    let password = '<h1>Password</h1><input id = "password" type="password" placeholder="123" required>';
-    $(content).append(first, last, email, username, password);
-
-    let submit = '<button id="signupbutton" class ="button" type = "submit">Sign Up</button>';
-    $(content).append(submit);
-
-    $(signUp).append(form);
-    $(form).append(content);
-
-    return signUp;
-}
-
-export const header = function() {
-    let header = $('<div class="header">');
-    let name = '<h1 class="appName">APP NAME</h1>';
-    $(header).append(name);
-    return header;
-}
-
-export const handleSubmitSignUp = function() {
-    let firstName= document.getElementById("first").value;
-    let lastName = document.getElementById("last").value;
-    let userName = document.getElementById("username").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-
-    firebase 
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then((user) => {
-            return users.doc(user.uid).set({
-                first: firstName,
-                last: lastName,
-                username: userName,
-            });
-        }).then(() => {
-            let form = document.getElementById("")
-        })
-    
-
-}
-
-export const renderMainPage = function() {
-    let root = $('#root');
-    
-}
-
-
-export const loadSignUpIntoDOM = function() {
-    const root = $('#root');
-    root.append(header());
-    root.append(signUpDisplay());
-
-    $('#signupbutton').on('click', function(event) {
-        handleSubmitSignUp(event);
-    });
-}
-
-
 $(function() {
-    loadSignUpIntoDOM();
+    let firstName= document.getElementById("first");
+    let lastName = document.getElementById("last");
+    let userName = document.getElementById("username");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let signupButton = document.getElementById("signup");
+    
+    signupButton.addEventListener("click", event => {
+        let firstN = firstName.value;
+        let lastN = lastName.value;
+        let userN = userName.value;
+        let mail = email.value;
+        let pass = password.value;
+        
+        firebase 
+        .auth()
+        .createUserWithEmailAndPassword(mail, pass)
+        .then((user) => {
+            db.collection("users").doc(user.uid).set({
+                first: firstN,
+                last: lastN,
+                username: userN,
+            });
+        }).catch(error => {
+            var errorCord = error.code;
+            var errorMessage = error.message;
+        });
+    });
+
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            let popMessage = '<span class="has-text-successful">You have successfully logged in with '+firebaseUser.email+'</span>,'
+                            '<div>Start browsing different artists from Spotify and add it to your collection! <a class="button start" href="./userpage.html"> Lets Go!';
+            document.html(popMessage);
+        } else {
+            let fail = '<span class="has-text-danger">You have not logged in successfully.</span>';
+            document.html(fail);
+        }
+    })
 });
