f65025e71f8a4d727ec8e81e03fb9fbfdaaf1012
Helen Tran <helentran@Helens-MacBook-Pro.local>
2020-12-04 14:29:01 -0500

    tried to set user info to db, auth still works

--- public/signup.js
+++ public/signup.js
@@ -5,6 +5,7 @@ $(function() {
 
     let firstName= document.getElementById("first");
     let lastName = document.getElementById("last");
+    let username = document.getElementById("username");
     let email = document.getElementById("email");
     let password = document.getElementById("password");
     let signupButton = document.getElementById("signup");
@@ -12,6 +13,7 @@ $(function() {
     signupButton.addEventListener("click", event => {
         let firstN = firstName.value;
         let lastN = lastName.value;
+        let userN = username.value;
         let mail = email.value;
         let pass = password.value;
         
@@ -19,28 +21,39 @@ $(function() {
         .auth()
         .createUserWithEmailAndPassword(mail, pass)
         .then((user) => {
-            if(user){ 
-                db.collection("users").doc(user.uid).set({
-                    first: firstN,
-                    last: lastN,
-                    username: userN,
-                });
-            }
-        }).catch((error) => {
+            // if(user){ 
+            //     db.collection("users").doc(user.uid).set({
+            //         first: firstN,
+            //         last: lastN,
+            //         username: userN,
+            //     });
+            // }
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
             var errorCord = error.code;
             var errorMessage = error.message;
         });
     }), true;
 
 
-    // firebase.auth().onAuthStateChanged(firebaseUser => {
-    //     if (firebaseUser) {
-    //         let popMessage = '<span class="has-text-successful">You have successfully logged in with '+firebaseUser.email+'</span>,'
-    //                         '<div>Start browsing different artists from Spotify and add it to your collection! <a class="button start" href="./userpage.html"> Lets Go!';
-    //         document.html(popMessage);
-    //     } else {
-    //         let fail = '<span class="has-text-danger">You have not logged in successfully.</span>';
-    //         document.html(fail);
-    //     }
-    // })
+        firebase.auth().onAuthStateChanged(firebaseUser => {
+            if (firebaseUser){
+                users.doc(this.firebaseUser.uid).get().then(function (doc) {
+                    if (!doc.exists){
+                        users.doc(this.firebaseUser.uid).set({
+                            email: firebaseUser.email,
+                            first: firstN,
+                            last: lastN,
+                            username: userN
+                        }).then(function () {
+                            window.location.href = 'index.html';
+                        }).catch(error => console.log(error.message));
+                    } else {
+                        window.location.href = 'index.html';
+                    }
+                })
+            } 
+        });
+    // }), true;
 });
