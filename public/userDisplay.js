88cfd71ad163254af53215e6df9e03a223bc4834
Helen Tran <helentran@helens-mbp.attlocal.net>
2020-12-08 16:03:55 -0500

    button changes

--- public/userDisplay.js
+++ public/userDisplay.js
@@ -1,24 +1,16 @@
-
-export const renderUserCard = function(doc) {
-    let result = $('#searchResult');
-    let userName = doc.username;
-    let name = '<div class="name" id="'+userName+'">'+userName+'</div>';
-
-    $(result).append(name);
-    return result;
-}
-
 export const renderEditUserCard = function(user) {
     let editCard = $('#editCard');
     let editInput = $('<div id="editInput">');
     let editContent = $('<form id="editForm"></form>');
     let editTitle = '<h1 class="editTitle">Edit Your Account Information</h1>';
     $(editInput).append(editTitle);
-    let editUserName = '<div class="field"><label class="editLabel">Username: </label><input class="editing" id="'+user.username+'" value="'+user.username+'"></div>';
-    let editFirst = '<div class"field"><label class="editLabel">First Name: </label><input class="editing" id="'+user.first+'" value="'+user.first+'"></div>';
-    let editLast = '<div class="field"><label class="editLabel">Last Name: </label><input class="editing" id="'+user.last+'" value="'+user.last+'"></div>';
-    let editEmail = '<div class="field"><label class="editLabel">Email: </label><input class= "editing" id="'+user.email+'" value="'+user.email+'"></div>';
-    $(editContent).append(editUserName, editFirst, editLast, editEmail);
+    let editUserName = '<div class="field"><label class="editLabel">Username: </label><input class="editing" id="newUsername" value="'+user.username+'"></div>';
+    let editFirst = '<div class"field"><label class="editLabel">First Name: </label><input class="editing" id="newFirst" value="'+user.first+'"></div>';
+    let editLast = '<div class="field"><label class="editLabel">Last Name: </label><input class="editing" id="newLast" value="'+user.last+'"></div>';
+    // let editEmail = '<div class="field"><label class="editLabel">Email: </label><input class= "editing" id="newEmail" value="'+user.email+'"></div>';
+    // let editPass ='<div class="field"><label class="editLabel">New Password: </label><input class="editing" id="newPass"></div>';
+    $(editContent).append(editUserName, editFirst, editLast);
+    // $(editContent).append(editEmail, editPass);
 
     let cancel = '<button id="cancelEdit" type="button">Cancel</button>';
     let save = '<button id="saveEdit" type="submit">Save</button>';
@@ -34,25 +26,46 @@ export const handleSubmitSearch = function(event) {
     firebase.firestore().collection("users").where("username", "==", searchingFor.value)
     .get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            let userData = doc.data();
-            renderUserCard(userData);
-        })
-    })
+            $('#searchResult').append('<div id="result"><h2 id="resultUser">This collection was created by '+doc.data().username+'></h2></div>');    
+        });
+    });
 }
 export const handleLogoutButton = function(event) {
-    event.preventDefault();
     firebase.auth().signOut();
     location.href = "/index.html";
 }
 
 export const handleEditButton = function(event) {
-    let user = event.target;
-    renderEditUserCard(user);
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            firebase.firestore().collection("users").doc(user.user.uid).get().then(function(doc) {
+                if (doc.exists) {
+                    renderEditUserCard(user);
+                }
+            })
+        }
+    })
 }
 
+export const handleSaveEditButton = function(event) {
+    let user = firebase.auth().currentUser;
+
+    let newFirst = document.getElementById("newFirst").value;
+    let newLast = document.getElementById("newLast").value;
+    let newUsername = document.getElementById("newUsername").value;
+    
+    user.updateProfile({
+        first: newFirst,
+        last: newLast,
+        username: newUsername,
+    }).then(function() {
+        location.href = "/userAccount.html";
+    });
+}
 
 $(document).ready(function() {
     $('#logout').on('click', handleLogoutButton);
     $('#submitSearch').on('click', handleSubmitSearch);
     $('#edit').on('click', handleEditButton);
+    $('#saveEdit').on('click', handleSaveEditButton);
 });
